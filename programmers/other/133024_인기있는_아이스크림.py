5e9eed3886f85c6a602db27d09c82d7d3b7b1c95
apqp2378 <apqp2378@gmail.com>
2026-01-16 15:07:55 +0900

    [프로그래머스] 인기있는 아이스크림 풀이 제출
    - 문제 ID: 133024
    - 난이도: unknown
    - 제출일: 2026. 01. 16. 15:07:53
    - 작성자: 최상원

---
+++ programmers/other/133024_인기있는_아이스크림.py
@@ -0,0 +1,11 @@
+# 인기있는 아이스크림
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/133024
+# 작성자: 최상원
+# 작성일: 2026. 01. 16. 15:07:53
+
+-- 코드를 입력하세요
+SELECT
+FLAVOR
+FROM FIRST_HALF 
+ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID ASC
