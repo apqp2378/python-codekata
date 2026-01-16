cb3f8d3b911bdba31fd64f3dc1889619a0154cb8
apqp2378 <apqp2378@gmail.com>
2026-01-16 16:45:59 +0900

    [프로그래머스] L0 - 나머지 구하기 (#5)

---
+++ programmers/L0_기초입문/입문/005_나머지_구하기.py
@@ -0,0 +1,9 @@
+# 나머지 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120810
+# 알고리즘: 기초
+# 작성자: 최상원
+# 작성일: 2026. 01. 16. 16:45:57
+
+def solution(num1, num2):
+    return num1 % num2
