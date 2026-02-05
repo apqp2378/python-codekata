c495935dc82f89853a87c98514a1da4ef1687602
apqp2378 <apqp2378@gmail.com>
2026-02-05 09:15:06 +0900

    [프로그래머스] L0 - 아이스 아메리카노 (#28)

---
+++ programmers/L0_기초입문/입문/028_아이스_아메리카노.py
@@ -0,0 +1,12 @@
+# 아이스 아메리카노
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120819
+# 알고리즘: 기초
+# 작성자: 최상원
+# 작성일: 2026. 02. 05. 09:15:06
+
+def solution(money):
+    answer = []
+    answer.append(money//5500)
+    answer.append(money%5500)
+    return answer
