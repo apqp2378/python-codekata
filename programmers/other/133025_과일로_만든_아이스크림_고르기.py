fc9e77fc2ccb75909e9eef73b5b74caa94fab3bf
apqp2378 <apqp2378@gmail.com>
2026-02-06 09:17:41 +0900

    [프로그래머스] 과일로 만든 아이스크림 고르기 풀이 제출
    - 문제 ID: 133025
    - 난이도: unknown
    - 제출일: 2026. 02. 06. 09:17:40
    - 작성자: 최상원

--- programmers/other/133025_과일로_만든_아이스크림_고르기.py
+++ programmers/other/133025_과일로_만든_아이스크림_고르기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/133025
 # 작성자: 최상원
-# 작성일: 2026. 02. 06. 09:14:26
+# 작성일: 2026. 02. 06. 09:17:40
 
 -- 코드를 입력하세요
 SELECT
@@ -12,3 +12,4 @@ join ICECREAM_INFO ii
 on f.FLAVOR = ii.FLAVOR
 where f.TOTAL_ORDER > 3000
 and ii.INGREDIENT_TYPE like '%fruit%'
+order by f.TOTAL_ORDER desc
