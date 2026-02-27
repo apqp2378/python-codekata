41b9aac60397601aea9957ebf4aa8a0d2fc15438
apqp2378 <apqp2378@gmail.com>
2026-02-27 09:19:38 +0900

    [프로그래머스] L0 - 짝수는 싫어요 (#40)

---
+++ programmers/L0_기초입문/입문/040_짝수는_싫어요.py
@@ -0,0 +1,9 @@
+# 짝수는 싫어요
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120813
+# 알고리즘: 기초
+# 작성자: 최상원
+# 작성일: 2026. 02. 27. 09:19:37
+
+def solution(n):
+    return list(range(1,n+1,2))
